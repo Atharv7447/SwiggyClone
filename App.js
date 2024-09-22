932fcb1aec3e3bfd60c78bcb4d7d157bc46c2ac3
Atharv <atharvramdasi01@gmail.com>
2024-09-22 23:27:11 +0530

    Added README file

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
+//Creating element in javascript
 // const root = document.getElementById("root");
 // const heading = document.createElement("h1");
 // heading.innerHTML = "Hello";
-
 // root.appendChild(heading);
-
+//**************************************************************** */
 // //Creating React core element
 // const heading = React.createElement("h1",{id:"heading"},"Hello World");
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
-
-
+//**************************************************************** */
 //creating mutiple/nested element like<div><div><h1>Hello World</h1></div></div>
+//To overcome this problem we use feature known as JSX
 const heading = React.createElement("div",{id:"parent"},
     [React.createElement("div",{id:"child1"},
         [React.createElement("h1",{},"I'm first sibling"),React.createElement("h1",{},"I'm second sibiling Tag")])
